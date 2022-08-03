d1980c1eeb8f00ae08b5500b33e6e03205f8c9fe
Felix Rieß <felix.riess@codecentric.de>
2022-08-03 15:40:42 +0200

    add empty consumer and producer

---
+++ kafka-playground-producer/src/main/kotlin/de/codecentric/kafka/playground/producer/KafkaPlaygroundProducerApplication.kt
@@ -0,0 +1,11 @@
+package de.codecentric.kafka.playground.producer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KafkaPlaygroundProducerApplication
+
+fun main(args: Array<String>) {
+	runApplication<KafkaPlaygroundProducerApplication>(*args)
+}
---
+++ kafka-playground-producer/src/test/kotlin/de/codecentric/kafka/playground/producer/KafkaPlaygroundProducerApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.codecentric.kafka.playground.producer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KafkaPlaygroundProducerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
